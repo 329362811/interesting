f51e0e25106901cd1013eda0473707fb86565629
KEVIN <329362811@qq.com>
2019-10-14 18:54:16 +0800

    修改框架

--- interesting/src/router/index.js
+++ interesting/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Table from '@/components/Table'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }, {
+    path: '/table',
+    name: 'table',
+    component: Table
+  }]
 })
